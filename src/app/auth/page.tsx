b85049be69b3d3acc77c770be976bc9e0ac59230
Arthur Brandao <arthurbrdantas@gmail.com>
2025-01-29 18:58:32 -0300

    chore: first commit

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import React, { FormEvent, useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Here you would typically handle the login logic, such as sending a request to your backend API
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
+  return (
+    <Container>
+      <Row className="justify-content-md-center mt-5">
+        <Col xs={12} md={6} lg={4}>
+          <Card>
+            <Card.Body>
+              <h2 className="text-center mb-4">Login</h2>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Form.Group>
+
+                <Button variant="primary" type="submit" className="w-100 mt-4">
+                  Login
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default LoginPage;
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+      nobis necessitatibus amet distinctio, rem, delectus rerum doloribus quo
+      nihil architecto possimus vitae sit labore voluptates nostrum numquam odio
+      harum corrupti?
+    </div>
+  );
+}
